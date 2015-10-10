c055f539ccf887f411c3fd3ad9b592eb0a4b60bc
rakuishi <rakuishi@gmail.com>
2015-10-10 10:01:51 +0900

    Use `Field.getGenericType()` to establish its data type

--- aileron/src/main/java/com/rakuishi/aileron/Aileron.java
+++ aileron/src/main/java/com/rakuishi/aileron/Aileron.java
@@ -53,10 +53,79 @@ public final class Aileron {
     public static void setValue(Object target, Field field, Bundle extras, String key)
             throws IllegalAccessException {
 
-        if (field.getType().equals(int.class)) {
-            field.setInt(target, extras.getInt(key));
-        } else if (field.getType().equals(String.class)) {
-            field.set(target, extras.getString(key));
+        String clazz = field.getGenericType().toString();
+
+        switch (clazz) {
+            case "byte":
+                field.setByte(target, extras.getByte(key));
+                break;
+            case "short":
+                field.setShort(target, extras.getShort(key));
+                break;
+            case "int":
+                field.setInt(target, extras.getInt(key));
+                break;
+            case "long":
+                field.setLong(target, extras.getLong(key));
+                break;
+            case "float":
+                field.setFloat(target, extras.getFloat(key));
+                break;
+            case "double":
+                field.setDouble(target, extras.getDouble(key));
+                break;
+            case "char":
+                field.setChar(target, extras.getChar(key));
+                break;
+            case "boolean":
+                field.setBoolean(target, extras.getBoolean(key));
+                break;
+            case "class [B":
+                field.set(target, extras.getByteArray(key));
+                break;
+            case "class [S":
+                field.set(target, extras.getShortArray(key));
+                break;
+            case "class [I":
+                field.set(target, extras.getIntArray(key));
+                break;
+            case "class [J":
+                field.set(target, extras.getLongArray(key));
+                break;
+            case "class [F":
+                field.set(target, extras.getFloatArray(key));
+                break;
+            case "class [D":
+                field.set(target, extras.getDoubleArray(key));
+                break;
+            case "class [C":
+                field.set(target, extras.getCharArray(key));
+                break;
+            case "class [Z":
+                field.set(target, extras.getBooleanArray(key));
+                break;
+            case "class java.lang.Byte":
+            case "class java.lang.Short":
+            case "class java.lang.Integer":
+            case "class java.lang.Long":
+            case "class java.lang.Float":
+            case "class java.lang.Double":
+            case "class java.lang.Character":
+            case "class java.lang.Boolean":
+                field.set(target, extras.get(key));
+                break;
+            case "java.util.List<java.lang.Integer>":
+                field.set(target, extras.getIntegerArrayList(key));
+                break;
+            case "class java.lang.String":
+                field.set(target, extras.getString(key));
+                break;
+            case "class [Ljava.lang.String;":
+                field.set(target, extras.getStringArray(key));
+                break;
+            case "java.util.List<java.lang.String>":
+                field.set(target, extras.getStringArrayList(key));
+                break;
         }
     }
 
