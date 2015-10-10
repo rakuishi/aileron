87cf75fb0c6d16f26746234dd15acb200b32bb47
rakuishi <rakuishi@gmail.com>
2015-10-10 09:05:21 +0900

    Support fragment

--- aileron/src/main/java/com/rakuishi/aileron/Aileron.java
+++ aileron/src/main/java/com/rakuishi/aileron/Aileron.java
@@ -1,6 +1,7 @@
 package com.rakuishi.aileron;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -14,10 +15,26 @@ public final class Aileron {
 
     public static void roll(Activity target) {
         Bundle extras = target.getIntent().getExtras();
-        if (extras == null) {
-            return;
+        if (extras != null) {
+            roll(target, extras);
         }
+    }
 
+    public static void roll(Fragment target) {
+        Bundle extras = target.getArguments();
+        if (extras != null) {
+            roll(target, extras);
+        }
+    }
+
+    public static void roll(android.support.v4.app.Fragment target) {
+        Bundle extras = target.getArguments();
+        if (extras != null) {
+            roll(target, extras);
+        }
+    }
+
+    public static void roll(Object target, Bundle extras) {
         for (Field field: target.getClass().getDeclaredFields()) {
             Extra extra = field.getAnnotation(Extra.class);
             if (extra == null) {
@@ -33,11 +50,13 @@ public final class Aileron {
         }
     }
 
-    public static void setValue(Activity target, Field field, Bundle extras, String key)
+    public static void setValue(Object target, Field field, Bundle extras, String key)
             throws IllegalAccessException {
 
         if (field.getType().equals(int.class)) {
             field.setInt(target, extras.getInt(key));
+        } else if (field.getType().equals(String.class)) {
+            field.set(target, extras.getString(key));
         }
     }
 
